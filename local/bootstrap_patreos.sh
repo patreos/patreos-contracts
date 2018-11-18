61c45525a974f979bbde0074a3c2a07554ad3df2
okayplanet <tyrick@gmail.com>
2018-11-18 14:15:42 -0700

    added run all script

--- local/bootstrap_patreos.sh
+++ local/bootstrap_patreos.sh
@@ -160,26 +160,3 @@ echo "${PATREOS_USERS[2]} Balance:"
 cleos get currency balance patreostoken ${PATREOS_USERS[2]}
 
 echo "Finished..."
-
-
-
-
-
-echo "Additional Setup for Simulation"
-
-# Setup
-echo "Transfering $EOS_TOKEN to account: ${PATREOS_USERS[3]}"
-JSON=$(jq -n --arg to "${PATREOS_USERS[3]}" --arg quantity "200.0000 $EOS_TOKEN" '{ from: "eosio", to: $to, quantity: $quantity, memo: "Gift from eosio" }')
-cleos push action eosio.token transfer "${JSON}" -p eosio
-
-echo "Transfering $EOS_TOKEN to account: ${PATREOS_USERS[4]}"
-JSON=$(jq -n --arg to "${PATREOS_USERS[4]}" --arg quantity "100.0000 $EOS_TOKEN" '{ from: "eosio", to: $to, quantity: $quantity, memo: "Gift from eosio" }')
-cleos push action eosio.token transfer "${JSON}" -p eosio
-
-echo "Transfering $PATREOS_TOKEN to account: ${PATREOS_USERS[3]}"
-JSON=$(jq -n --arg from "${PATREOS_USERS[0]}" --arg to "${PATREOS_USERS[3]}" --arg quantity "1000.0000 $PATREOS_TOKEN" '{ from: $from, to: $to, quantity: $quantity, memo: "Gift from eosio" }')
-cleos push action patreostoken transfer "${JSON}" -p ${PATREOS_USERS[0]}
-
-echo "Transfering $PATREOS_TOKEN to account: ${PATREOS_USERS[4]}"
-JSON=$(jq -n --arg from "${PATREOS_USERS[0]}" --arg to "${PATREOS_USERS[4]}" --arg quantity "2000.0000 $PATREOS_TOKEN" '{ from: $from, to: $to, quantity: $quantity, memo: "Gift from eosio" }')
-cleos push action patreostoken transfer "${JSON}" -p ${PATREOS_USERS[0]}
---
+++ local/run_all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./bootstrap_patreos.sh
+
+./sim_setup.sh
+
+./sim_patreos_users.sh
